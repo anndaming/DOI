1c2a31a8c74245cf89fc371044095cbc398a3338
陈国瑞 <graig@imeigoo.cn>
2023-06-29 17:23:20 +0800

    Add background for Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import {Input, notification} from 'antd';
 import qs from "querystring";
 import styles from './index.module.css'
 import {useTranslation} from 'react-i18next';
+import {homeBackgroundImage} from "../resource/resources";
 
 const Search = Input.Search;
 
@@ -22,7 +23,7 @@ const Home = observer(function () {
         window.location.href = `/WorksDOI?${qs.stringify({q: value})}`
     }
 
-    return <div className={styles.container}>
+    return <div className={styles.container} style={{backgroundImage: `url(${homeBackgroundImage})`}}>
         <div className={styles.inner}>
             <p className={styles.searchDescription}>Search the metadata of journal articles, books, standards, datasets & more</p>
             <Search
---
+++ src/resource/resources.ts
@@ -0,0 +1,1 @@
+export const homeBackgroundImage = require("./home_background.jpeg");
